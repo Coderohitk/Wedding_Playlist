7abf6023ac3a54e3cced04234d4ccef79f742a56
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2025-04-08 17:16:26 -0400

    updated

--- Wedding_Playlist/Wedding_Playlist/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Wedding_Playlist.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
