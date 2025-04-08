baecc88218f744c9b7cddd74e12ef51d6b47c11f
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2025-04-08 17:32:55 -0400

    updated'

---
+++ Wedding_Playlist/Wedding_Playlist/Models/PlaylistSong.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wedding_Playlist.Models
+{
+    public class PlaylistSong
+    {
+        [Key]
+        public int PlaylistSongId { get; set; }
+        public int Order { get; set; }
+
+        public int PlaylistID { get; set; }
+
+
+        public int SongID { get; set; }
+
+        public Playlist Playlist { get; set; }
+        public Song Song { get; set; }
+    }
+    public class PlaylistSongDTO
+    {
+        public int PlaylistSongId { get; set; }
+        public int Order { get; set; }
+        public int PlaylistID { get; set; }
+        public int SongID { get; set; }
+    }
+}
